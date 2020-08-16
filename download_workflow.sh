5688ca63dbe4048a46744d907bebde1703b6c69c
Melissa Chen <melissachen@Melissas-MacBook-Pro.local>
2020-08-16 13:35:39 -0600

    plugging away at scan

--- download_workflow.sh
+++ download_workflow.sh
@@ -1,3 +1,1 @@
 #!bin/bash
-
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=sample&query=environmental_package%3D%22host-associated%22&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search"
